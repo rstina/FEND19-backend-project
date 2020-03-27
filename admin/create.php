caa3535051e65c3a9e73012babe05ed0f63ba5eb
rstina <53047495+rstina@users.noreply.github.com>
2020-03-27 15:37:43 +0100

    create

---
+++ admin/create.php
@@ -0,0 +1,18 @@
+<?php
+/**************************************** *
+ * filename: update.php
+ * author: Stina Englesson & Jing-Jing Lin
+ * date 2020-03-27
+ * 
+ * create blog-post
+**************************************** */
+
+  include_once '../header.php';
+?>
+
+
+<h1>Skapa blogginlägg</h1>
+
+<?php
+  include_once '../footer.php';
+?>
