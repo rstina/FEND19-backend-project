7a6d751abe90b2a72b72260721ce4a35d7d63204
JingJing Lin <jlina.lin@gmail.com>
2020-03-27 15:36:48 +0100

    index for admin

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php
+/**************************************** *
+ * filename: index.php
+ * author: Stina Englesson & Jing-Jing Lin
+ * date 2020-03-27
+ * 
+ * landing page for admin
+**************************************** */
+
+include_once '../db.php';
+include_once '../header.php';
+
+?>
+
+<h2>All blog posts</h2>
+
+<?php 
+include_once '../read.php';
+include_once '../footer.php'; 
+?>
