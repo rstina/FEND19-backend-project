e165ef21232d3d903bf6f4badbbe92f4258a29bd
JingJing Lin <jlina.lin@gmail.com>
2020-03-27 14:12:58 +0100

    add datebase

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+$db_server   = "localhost"; // adress till dator server
+$db_database = "contactsdb"; // ändras alltid
+$db_username = "root"; // ändras alltid
+$db_password = "root"; // ändras alltid
+
+try{ // skapar objekt 
+  $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8" 
+                , $db_username 
+                , $db_password);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION); 
+
+  // echo "<h2>Connected Successfully</h2>";
+
+  // vid minsta fel, catch och få error 
+}catch(PDOException $e){
+  echo "<h2>Error: " . $e-> getMessage() . "</h2>";
+}
