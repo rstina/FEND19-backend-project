a18a7ca7bcc8883428bd6672a02404bb8653c11b
rstina <53047495+rstina@users.noreply.github.com>
2020-03-30 16:21:07 +0200

    update working

---
+++ admin/edit.php
@@ -0,0 +1,94 @@
+<?php
+  include_once '../db.php';
+  include_once '../header-admin.php';
+
+  $id = htmlentities($_GET['id']);
+
+  // hämtar infon från ordern
+  $sql = 'SELECT * FROM blog WHERE id = :id';
+
+  $stmt = $db->prepare($sql);
+  $stmt->bindParam(':id', $id);
+  $stmt->execute();
+  
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  $heading = htmlspecialchars($row['heading']);
+  $content = htmlspecialchars($row['content']);
+  $map = htmlspecialchars($row['map']);
+  $video = htmlspecialchars($row['video']);
+  $image = htmlspecialchars($row['image']);
+  $publish = htmlspecialchars($row['publish']);
+
+
+?>
+  
+  <h1>Uppdatera blogginlägg</h1>
+
+<form   action="update.php?id=<?php echo $id;?>"       
+        enctype="multipart/form-data"  
+        method="post" 
+        class="row">
+
+        <div class="col-md-12 form-group">
+            <input name="heading" type="text" required
+            class="form-control" placeholder="Rubrik" value="<?php echo $heading ?>">
+        </div>   
+      
+        <div class="col-md-12 form-group">
+            <textarea name="content" cols="30" rows="5" required
+            class="form-control" placeholder="Skriv ett inlägg" value="<?php echo $content ?>"></textarea>
+        </div>
+
+        <div class="col-md-12 form-group">
+            <input name="map" type="text" 
+            class="form-control" placeholder="Klistra in länken till karta" value="<?php echo $map ?>">
+        </div>   
+        
+        <br>
+        <div class="col-md-12 form-group">
+            <input name="video" type="text" 
+            class="form-control" placeholder="Klistra in länken till video" value="<?php echo $video ?>">
+        </div>  
+
+            
+        <br>
+        <div class="col-md-12 form-group">
+            <input  type="file" 
+                    name="image" 
+                    id="fileToUpload" 
+                    value="<?php echo $image ?>"
+                    class="form-control"> <!-- type="file" ger en file-select knapp i input -->
+        
+        </div>
+
+
+        <div class="col-md-12 form-group">
+       <input type="radio" id="publish" name="publish" value="<?php echo $publish ?>">
+       <label for="publish">Publicera</label><br>
+       <input type="radio" id="publish" name="publish" value="<?php echo $publish ?>">
+       <label for="publish">Avpublicera</label><br>
+       </div>
+
+
+        <div class="col-md-12 form-group">
+            <input  type="submit" 
+                    value="Updatera inlägget"
+                    class="btn btn-success form-control">
+        </div>
+    </form>
+
+
+<?php
+  include_once '../footer.php';
+?>
+
+    <?php
+require_once '../footer.php';
+          // <form action='update.php?id=$id'>
+          //   <input type='text' class='form-control' name='name' id='changeName' value='$name'>
+          // </form>
+
+          // <a href='update.php?id=$id' class='form-control'>Skicka</a>
+
+?>
--- admin/update.php
+++ admin/update.php
@@ -1,140 +1,15 @@
 <?php
-/**************************************** *
- * filename: update.php
- * author: Stina Englesson & Jing-Jing Lin
- * date 2020-03-27
- * 
- * update blog-post
-**************************************** */
+include_once '../db.php';
 
-include_once '../header-admin.php';
+$customerID = htmlentities($_GET['id']);
+$heading = htmlentities($_POST['heading']);
+echo $heading;
 
-require_once '../db.php';
+$sql = "UPDATE blog
+SET heading = '$heading'
+WHERE id = $customerID;";
 
-if(isset($_GET['id'])){
-  $id = htmlspecialchars($_GET['id']);
-  $sql = "SELECT * FROM blog WHERE id = :id";
-  $stmt = $db->prepare($sql);
-  $stmt->bindParam(':id' , $id );
-  $stmt->execute();
+$stmt = $db->prepare($sql);
+$stmt->execute();
 
-  if($stmt->rowCount() > 0){
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $heading  = $row['heading'];
-    $image = $row['image'];
-    $content = $row['content'];
-    $date = $row['date'];
-    $map = $row['map'];
-    $video = $row['video'];
-    $publish = $row['publish'];
-
-
-  }else{
-    header('Location:index.php');
-    exit;
-  }
-
-}else{
-  header('Location:index.php');
-  exit;
-}
-
-// 2. Uppdatera databasen (raden) med ny data från formuläret
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
- $heading = htmlspecialchars($_POST['heading']);
-  $content = htmlspecialchars($_POST['content']);
-  $map = htmlspecialchars($_POST['map']);
-  $video = htmlspecialchars($_POST['video']);
-  $image = htmlspecialchars(basename( $_FILES["image"]["name"]));
-  $publish = htmlspecialchars($_POST['publish']);
-
-
-  $sql = "UPDATE blog
-          SET heading = :heading, content = :content , map = :map , video = :video , image = :image , publish = :publish   
-          WHERE id = :id";
-
-  $stmt = $db->prepare($sql);
-
-  $stmt->bindParam(':heading', $heading);
-  $stmt->bindParam(':content' , $content);
-  $stmt->bindParam(':map'  , $map);
-  $stmt->bindParam(':video'  , $video);
-  $stmt->bindParam(':image'  , $image);
-  $stmt->bindParam(':publish'  , $publish);
-
-  $stmt->execute();
-  header('Location:index.php');
-  exit;
-}
-
-?>
-
-
-
-<h1>Uppdatera blogginlägg</h1>
-
-<form   action="upload.php"       
-        enctype="multipart/form-data"  
-        method="post" 
-        class="row">
-
-        <div class="col-md-12 form-group">
-            <input name="heading" type="text" required
-            class="form-control" placeholder="Rubrik" value="<?php echo $heading ?>">
-        </div>   
-      
-        <div class="col-md-12 form-group">
-            <textarea name="content" cols="30" rows="5" required
-            class="form-control" placeholder="Skriv ett inlägg" value="<?php echo $content ?>"></textarea>
-        </div>
-
-        <div class="col-md-12 form-group">
-            <input name="map" type="text" 
-            class="form-control" placeholder="Klistra in länken till karta" value="<?php echo $map ?>">
-        </div>   
-        
-        <br>
-        <div class="col-md-12 form-group">
-            <input name="video" type="text" 
-            class="form-control" placeholder="Klistra in länken till video" value="<?php echo $video ?>">
-        </div>  
-        
-        <!-- <br>
-        <div class="col-md-12 form-group">
-            <input name="video" type="text" required
-            class="form-control" placeholder="Ladda upp bild">
-        </div>   -->
-
-            
-        <br>
-        <div class="col-md-12 form-group">
-            <input  type="file" 
-                    name="image" 
-                    id="fileToUpload" 
-                    value="<?php echo $image ?>"
-                    class="form-control"> <!-- type="file" ger en file-select knapp i input -->
-        
-        </div>
-
-
-        <div class="col-md-12 form-group">
-       <input type="radio" id="publish" name="publish" value="<?php echo $publish ?>">
-       <label for="publish">Publicera</label><br>
-       <input type="radio" id="publish" name="publish" value="<?php echo $publish ?>">
-       <label for="publish">Avpublicera</label><br>
-       </div>
-
-
-        <div class="col-md-12 form-group">
-            <input  type="submit" 
-                    value="Updatera inlägget"
-                    class="btn btn-success form-control">
-        </div>
-    </form>
-
-
-<?php
-  include_once '../footer.php';
-?>
+header('Location: index.php');
